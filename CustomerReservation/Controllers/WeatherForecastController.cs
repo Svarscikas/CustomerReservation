011bfcae134adff859730ab44786808b2aa3da01
Arnas <arnas.e@gmail.com>
2023-08-14 21:35:21 +0300

    Initial commit

--- CustomerReservation/Controllers/WeatherForecastController.cs
+++ CustomerReservation/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ namespace CustomerReservation.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly string[] Summaries = new []
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
